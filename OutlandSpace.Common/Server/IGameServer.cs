1003d359a38949c12beca69dc8927c528693b368
SuhilovVitaly <98092169+SuhilovVitaly@users.noreply.github.com>
2022-06-18 09:47:17 +0300

    Game server interfaces initialization

---
+++ OutlandSpace.Common/Server/IGameServer.cs
@@ -0,0 +1,17 @@
+﻿using OutlandSpace.Common.Server.Session;
+
+namespace OutlandSpace.Common.Server;
+
+public interface IGameServer
+{
+    IGameSessionData RefreshGameSession(int id);
+    void ResumeSession(int id);
+
+    void PauseSession(int id);
+
+    void Command(int sessionId, string command);
+
+    int GetTurn(int sessionId);
+
+    IGameSessionData Execute(int sessionId, int turns);
+}
---
+++ OutlandSpace.Common/Server/Session/IGameSessionData.cs
@@ -0,0 +1,24 @@
+﻿using OutlandSpace.Universe.CelestialObjects;
+
+namespace OutlandSpace.Common.Server.Session;
+
+public interface IGameSessionData
+{
+    int Id { get; set; }
+
+    int Turn { get; }
+
+    int Step { get; set; }
+
+    bool IsPause { get; set; }
+
+    string ScenarioName { get; set; }
+
+    bool IsValid { get; set; }
+
+    DateTime LastUpdate { get; set; }
+
+    DateTime ExecuteTime { get; set; }
+
+    List<ICelestialObject> GetCelestialObjects();
+}
